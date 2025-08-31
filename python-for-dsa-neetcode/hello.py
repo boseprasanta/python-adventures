04d967bb7555ce94ba235b601cd44acdafb1c77c
pbose1310@gmail.com <prasanta@192.168.16.9>
2025-08-31 14:08:03 +0530

    Add initial implementation of variable assignment and increment examples

---
+++ python-for-dsa-neetcode/hello.py
@@ -0,0 +1,34 @@
+# variable are dynamically typed
+
+# basic assignment and print
+
+n = 13
+
+print("n =",n)
+
+n = "abc"
+
+print("n =", n)
+
+
+# multiple assignments
+
+a, b = "hi there", 98
+
+
+print("a =", a)
+print("b =", b)
+
+# increment
+
+i = 0
+
+i += 1 # Good
+print("i =", i)
+i = i + 1 # Good
+print("i =", i)
+
+# i++ bad
+
+
+
